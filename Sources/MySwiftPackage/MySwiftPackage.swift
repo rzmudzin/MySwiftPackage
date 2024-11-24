3712e9fb5c135f52398f917c666c59f27bad6bad
rob <rob@rob.com>
2024-11-24 08:26:17 -0700

    Adding basic view

--- Sources/MySwiftPackage/MySwiftPackage.swift
+++ Sources/MySwiftPackage/MySwiftPackage.swift
@@ -1,3 +1,5 @@
+//import Starscream
+
 public struct MySwiftPackage {
     public private(set) var text = "Hello, World!"
 
@@ -5,3 +7,38 @@ public struct MySwiftPackage {
         print("Init called")
     }
 }
+
+
+/*
+ import PackageDescription
+
+ let package = Package(
+     name: "MySwiftPackage",
+     products: [
+         // Products define the executables and libraries a package produces, and make them visible to other packages.
+         .library(
+             name: "MySwiftPackage",
+             targets: ["MySwiftPackage"]),
+     ],
+     dependencies: [
+         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1")
+         // Dependencies declare other packages that this package depends on.
+         // .package(url: /* package url */, from: "1.0.0"),
+     ],
+     targets: [
+         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+         .target(
+             name: "MySwiftPackage",
+             dependencies: ["Starscream"],
+             path: "Sources",
+             resources: [
+                 .process("Resource/Media.xcassets")
+             ]),
+         .testTarget(
+             name: "MySwiftPackageTests",
+             dependencies: ["MySwiftPackage"]),
+     ]
+ )
+
+ */
---
+++ Sources/Views/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rob Zmudzinshi on 11/24/24.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct SwiftUIView: View {
+    public init() {}
+    public var body: some View {
+        Text("THIS IS THE SWIFTUIVIEW FROM THE PACKAGE")
+//        Image("starscream")
+    }
+}
